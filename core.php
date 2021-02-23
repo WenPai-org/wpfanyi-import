4827dfb02ac7fed288fdc51dedf13045f20ddd72
sunxiyuan <sxy@ibadboy.net>
2021-02-23 23:06:20 +0800

    feat:Add accordion

--- core.php
+++ core.php
@@ -82,31 +82,29 @@ class WPfanyi_Import {
      */
     public function register_css_and_js() {
         echo <<<EOT
+<link  href="https://lib.baomitu.com/layui/2.5.7/css/layui.min.css" rel="stylesheet">
 <style>
-    .trans_tabs {
-        display: flex;
-        list-style: none;
-    }
-
-    .trans_tabs li {
-        padding: .5rem 1rem 1rem;
-        margin: 0 1rem;
-        cursor: pointer;
-    }
-
-    .trans_tabs li.active, .trans_tabs li:hover {
-        box-shadow: inset 0 -3px #007cba;
+.layui-tab-title li{
+    margin: 0 2em;
+}
+.layui-tab-title .layui-this:after{
+        border: 0;
+   box-shadow: inset 0 -0px #007cba;
+}
+.help .layui-show{
+        background: #fff;
+}
+.layui-colla-title{
+    
+    box-shadow: #00000005 1px 1px 8px;
+}
+    .layui-tab-title li.layui-this, .layui-tab-title li:hover {
+        box-shadow: inset 0 -2px #007cba;
 
         transition: box-shadow .5s ease-in-out;
     }
 
-    .tab-item {
-        display: none;
-    }
 
-    .show {
-        display: block;
-    }
 	#trans_url{min-height: 31px;
 		width: 28em;}
 	.form-table th{
@@ -114,35 +112,35 @@ class WPfanyi_Import {
 	}
 	.input_clear button{
 	    position: absolute;
-    right: 5px;
+    right: 8px;
     margin: auto;
     bottom: 0;
     top: 0;
     border: 0;
     background: none;
-    font-size: 18px;
     cursor: pointer;
 	}
+	.right{}
 </style>
-<script >!function(a){function b(a){return"[id="+a.attr("id")+"][name="+a.attr("name")+"]"}a.fn.sisyphus=function(c){var d=a.map(this,function(c){return b(a(c))}).join(),e=Sisyphus.getInstance(d);return e.protect(this,c),e};var c={};c.isAvailable=function(){if("object"==typeof a.jStorage)return!0;try{return localStorage.getItem}catch(b){return!1}},c.set=function(b,c){if("object"==typeof a.jStorage)a.jStorage.set(b,c+"");else try{localStorage.setItem(b,c+"")}catch(d){}},c.get=function(b){if("object"==typeof a.jStorage){var c=a.jStorage.get(b);return c?c.toString():c}return localStorage.getItem(b)},c.remove=function(b){"object"==typeof a.jStorage?a.jStorage.deleteKey(b):localStorage.removeItem(b)},Sisyphus=function(){function f(){return{setInstanceIdentifier:function(a){this.identifier=a},getInstanceIdentifier:function(){return this.identifier},setInitialOptions:function(b){var d={excludeFields:[],customKeySuffix:"",locationBased:!1,timeout:0,autoRelease:!0,onBeforeSave:function(){},onSave:function(){},onBeforeRestore:function(){},onRestore:function(){},onRelease:function(){}};this.options=this.options||a.extend(d,b),this.browserStorage=c},setOptions:function(b){this.options=this.options||this.setInitialOptions(b),this.options=a.extend(this.options,b)},protect:function(b,c){this.setOptions(c),b=b||{};var f=this;if(this.targets=this.targets||[],f.options.name?this.href=f.options.name:this.href=location.hostname+location.pathname+location.search+location.hash,this.targets=a.merge(this.targets,b),this.targets=a.unique(this.targets),this.targets=a(this.targets),!this.browserStorage.isAvailable())return!1;var g=f.options.onBeforeRestore.call(f);if((void 0===g||g)&&f.restoreAllData(),this.options.autoRelease&&f.bindReleaseData(),!d.started[this.getInstanceIdentifier()])if(f.isCKEditorPresent())var h=setInterval(function(){e.isLoaded&&(clearInterval(h),f.bindSaveData(),d.started[f.getInstanceIdentifier()]=!0)},100);else f.bindSaveData(),d.started[f.getInstanceIdentifier()]=!0},isCKEditorPresent:function(){return this.isCKEditorExists()?(e.isLoaded=!1,e.on("instanceReady",function(){e.isLoaded=!0}),!0):!1},isCKEditorExists:function(){return"undefined"!=typeof e},findFieldsToProtect:function(a){return a.find(":input").not(":submit").not(":reset").not(":button").not(":file").not(":password").not(":disabled").not("[readonly]")},bindSaveData:function(){var c=this;c.options.timeout&&c.saveDataByTimeout(),c.targets.each(function(){var d=b(a(this));c.findFieldsToProtect(a(this)).each(function(){if(-1!==a.inArray(this,c.options.excludeFields))return!0;var e=a(this),f=(c.options.locationBased?c.href:"")+d+b(e)+c.options.customKeySuffix;(e.is(":text")||e.is("textarea"))&&(c.options.timeout||c.bindSaveDataImmediately(e,f)),c.bindSaveDataOnChange(e)})})},saveAllData:function(){var c=this;c.targets.each(function(){var d=b(a(this)),f={};c.findFieldsToProtect(a(this)).each(function(){var g=a(this);if(-1!==a.inArray(this,c.options.excludeFields)||void 0===g.attr("name")&&void 0===g.attr("id"))return!0;var h=(c.options.locationBased?c.href:"")+d+b(g)+c.options.customKeySuffix,i=g.val();if(g.is(":checkbox")){var j=g.attr("name");if(void 0!==j&&-1!==j.indexOf("[")){if(f[j]===!0)return;i=[],a("[name='"+j+"']:checked").each(function(){i.push(a(this).val())}),f[j]=!0}else i=g.is(":checked");c.saveToBrowserStorage(h,i,!1)}else if(g.is(":radio"))g.is(":checked")&&(i=g.val(),c.saveToBrowserStorage(h,i,!1));else if(c.isCKEditorExists()){var k=e.instances[g.attr("name")]||e.instances[g.attr("id")];k?(k.updateElement(),c.saveToBrowserStorage(h,g.val(),!1)):c.saveToBrowserStorage(h,i,!1)}else c.saveToBrowserStorage(h,i,!1)})}),c.options.onSave.call(c)},restoreAllData:function(){var c=this,d=!1;c.targets.each(function(){var e=a(this),f=b(a(this));c.findFieldsToProtect(e).each(function(){if(-1!==a.inArray(this,c.options.excludeFields))return!0;var e=a(this),g=(c.options.locationBased?c.href:"")+f+b(e)+c.options.customKeySuffix,h=c.browserStorage.get(g);null!==h&&(c.restoreFieldsData(e,h),d=!0)})}),d&&c.options.onRestore.call(c)},restoreFieldsData:function(a,b){if(void 0===a.attr("name")&&void 0===a.attr("id"))return!1;var c=a.attr("name");!a.is(":checkbox")||"false"===b||void 0!==c&&-1!==c.indexOf("[")?!a.is(":checkbox")||"false"!==b||void 0!==c&&-1!==c.indexOf("[")?a.is(":radio")?a.val()===b&&a.prop("checked",!0):void 0===c||-1===c.indexOf("[")?a.val(b):(b=b.split(","),a.val(b)):a.prop("checked",!1):a.prop("checked",!0)},bindSaveDataImmediately:function(a,b){var c=this;if("onpropertychange"in a?a.get(0).onpropertychange=function(){c.saveToBrowserStorage(b,a.val())}:a.get(0).oninput=function(){c.saveToBrowserStorage(b,a.val())},this.isCKEditorExists()){var d=e.instances[a.attr("name")]||e.instances[a.attr("id")];d&&d.document.on("keyup",function(){d.updateElement(),c.saveToBrowserStorage(b,a.val())})}},saveToBrowserStorage:function(a,b,c){var d=this,e=d.options.onBeforeSave.call(d);(void 0===e||e!==!1)&&(c=void 0===c?!0:c,this.browserStorage.set(a,b),c&&""!==b&&this.options.onSave.call(this))},bindSaveDataOnChange:function(a){var b=this;a.change(function(){b.saveAllData()})},saveDataByTimeout:function(){var a=this,b=a.targets;setTimeout(function(){function b(){a.saveAllData(),setTimeout(b,1e3*a.options.timeout)}return b}(b),1e3*a.options.timeout)},bindReleaseData:function(){var c=this;c.targets.each(function(){var d=a(this),e=b(d);a(this).bind("submit reset",function(){c.releaseData(e,c.findFieldsToProtect(d))})})},manuallyReleaseData:function(){var c=this;c.targets.each(function(){var d=a(this),e=b(d);c.releaseData(e,c.findFieldsToProtect(d))})},releaseData:function(c,e){var f=!1,g=this;d.started[g.getInstanceIdentifier()]=!1,e.each(function(){if(-1!==a.inArray(this,g.options.excludeFields))return!0;var d=a(this),e=(g.options.locationBased?g.href:"")+c+b(d)+g.options.customKeySuffix;g.browserStorage.remove(e),f=!0}),f&&g.options.onRelease.call(g)}}}var d={instantiated:[],started:[]},e=window.CKEDITOR;return{getInstance:function(a){return d.instantiated[a]||(d.instantiated[a]=f(),d.instantiated[a].setInstanceIdentifier(a),d.instantiated[a].setInitialOptions()),a?d.instantiated[a]:d.instantiated[a]},free:function(){return d={instantiated:[],started:[]},null},version:"1.1.3"}}()}(jQuery);</script>
+<script  src="//lib.baomitu.com/sisyphus.js/latest/sisyphus.min.js"></script>
+<script  src="//lib.baomitu.com/layui/2.5.7/layui.min.js"></script>
 <script type="text/javascript">
   (function ($) {
-  
-  
-    $(".trans_tabs li").click(function () {
-      // 修改tab标签样式
-      $(this).attr("class", "active")
-      $(this).siblings().attr("class", "")
 
+$(".layui-tab-title li").click(function(){
+		var picTabNum = $(this).index();
+		sessionStorage.setItem("picTabNum",picTabNum);
+		var getPicTabNum = sessionStorage.getItem("picTabNum");
+		$(".layui-tab-title li").eq(getPicTabNum).addClass("layui-this").siblings().removeClass("layui-this");
+		$(".layui-tab-content>div").eq(getPicTabNum).addClass("layui-show").siblings().removeClass("layui-show");
+	});
+	$(function(){
+		var getPicTabNum = sessionStorage.getItem("picTabNum");
+		$(".layui-tab-title li").eq(getPicTabNum).addClass("layui-this").siblings().removeClass("layui-this");
+		$(".layui-tab-content>div").eq(getPicTabNum).addClass("layui-show").siblings().removeClass("layui-show");
+	})
 
-
-      // 获取tab ID
-      var itemId = $(this).attr("tabid") - 1;
-
-      // 切换到指定tab
-      $("#box").find("div:eq(" + itemId + ")").attr("class", "show")
-      $("#box").find("div:eq(" + itemId + ")").siblings().attr("class", "tab-item")
-    })
+   
 	
 
 $( "form" ).sisyphus();
@@ -162,11 +160,12 @@ $(".input_clear").click(function(){
 
 
 
+layui.use('element', function(){
+  var element = layui.element;
 
-
+});
   })(jQuery);
 </script>
-
 EOT;
     }
 
--- page.php
+++ page.php
@@ -13,87 +13,117 @@ defined( 'ABSPATH' ) || exit;
     <div class="notice notice-info">
       <p>
           <?php esc_html_e('The translation pack is a Zip package including MO and PO files. Select the translation pack on this page and set its type correctly then click Import to add it to WordPress.', 'wpfanyi-import'); ?><br/>
-        <b><?php esc_html_e('Note: If a translation package with the same name already exists, this operation will overwrite it ', 'wpfanyi-import'); ?></b>
+        <b><?php esc_html_e('Note: If a translation package with the same name already exists, this operation will overwrite it', 'wpfanyi-import'); ?></b>
       </p>
     </div>
-    <ul class="trans_tabs">
-      <li tabid="1" class="active"><?php esc_html_e('Import from Local', 'wpfanyi-import'); ?></li>
-      <li tabid="2"><?php esc_html_e('Import from URL', 'wpfanyi-import'); ?></li>
-    </ul>
+    <div class="main">
+      <div class="function">
 
-    <div id="box">
-      <div class="tab-item show">
-        <form method="post" enctype="multipart/form-data">
-            <?php wp_nonce_field('wpfanyi-import-nonce'); ?>
-          <input type="hidden" name="trans_import_method" value="file">
-          <table class="form-table" role="presentation">
-            <tbody>
-            <tr>
-              <th><?php esc_html_e('Translation package:', 'wpfanyi-import'); ?></th>
-              <td>
-                <label>
-                  <input type="file" name="trans_zip"/ accept=".zip">
-                </label>
-              </td>
-            </tr>
-            <tr>
-              <th><?php esc_html_e('Package type:', 'wpfanyi-import'); ?></th>
-              <td>
-                <label>
-                  <input type="radio" name="trans_type" value="plugin" checked/>
-                      <?php esc_html_e('Plugin', 'wpfanyi-import'); ?></label>
-                <label>
-                  <input type="radio" name="trans_type" value="theme"/>
-                    <?php esc_html_e('Theme', 'wpfanyi-import'); ?></label>
-              </td>
-            </tr>
-            </tbody>
-          </table>
-          <p class="submit">
-            <input type="submit" name="submit" class="button-primary"
-                   value="<?php esc_html_e('Import', 'wpfanyi-import'); ?>"/>
-          </p>
-        </form>
-      </div>
-      <div class="tab-item">
-        <form method="post" enctype="multipart/form-data">
-            <?php wp_nonce_field('wpfanyi-import-nonce'); ?>
-          <input type="hidden" name="trans_import_method" value="url">
-          <table class="form-table" role="presentation">
-            <tbody>
-            <tr>
-              <th scope="row"><?php esc_html_e('URL address:', 'wpfanyi-import'); ?></th>
-              <td>
-				  
-<div style="display:inline-block;position:relative;">  
-             <div  class="input_clear">
-		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">
-					×
-		</button>
-	     </div>  
-             <input name="trans_url" type="url" id="trans_url" value="" class="trans_url" placeholder="https://wpfanyi.com/glotpress/bulk-export/wordfence/">
+        <ul class="layui-tab-title">
+          <li class="layui-this"><?php esc_html_e('Import from Local', 'wpfanyi-import'); ?></li>
+          <li><?php esc_html_e('Import from URL', 'wpfanyi-import'); ?></li>
+        </ul>
+
+        <div class="layui-tab-content">
+          <div class="layui-tab-item layui-show">
+            <form method="post" enctype="multipart/form-data">
+                <?php wp_nonce_field('wpfanyi-import-nonce'); ?>
+              <input type="hidden" name="trans_import_method" value="file">
+              <table class="form-table" role="presentation">
+                <tbody>
+                <tr>
+                  <th><?php esc_html_e('Translation package:', 'wpfanyi-import'); ?></th>
+                  <td>
+                    <label>
+                      <input type="file" name="trans_zip"/ accept=".zip">
+                    </label>
+                  </td>
+                </tr>
+                <tr>
+                  <th><?php esc_html_e('Package type:', 'wpfanyi-import'); ?></th>
+                  <td>
+                    <label>
+                      <input type="radio" name="trans_type" value="plugin" checked/>
+                        <?php esc_html_e('Plugin', 'wpfanyi-import'); ?></label>
+                    <label>
+                      <input type="radio" name="trans_type" value="theme"/>
+                        <?php esc_html_e('Theme', 'wpfanyi-import'); ?></label>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+              <p class="submit">
+                <input type="submit" name="submit" class="button-primary"
+                       value="<?php esc_html_e('Import', 'wpfanyi-import'); ?>"/>
+              </p>
+            </form>
+          </div>
+          <div class="layui-tab-item">
+            <form method="post" enctype="multipart/form-data">
+                <?php wp_nonce_field('wpfanyi-import-nonce'); ?>
+              <input type="hidden" name="trans_import_method" value="url">
+              <table class="form-table" role="presentation">
+                <tbody>
+                <tr>
+                  <th scope="row"><?php esc_html_e('URL address:', 'wpfanyi-import'); ?></th>
+                  <td>
+                    <div style="display:inline-block;position:relative;">
+                      <div  class="input_clear">
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">
+                          ×
+                        </button>
+                      </div>
+                      <input name="trans_url" type="url" id="trans_url" value="" class="trans_url" placeholder="https://wpfanyi.com/glotpress/bulk-export/wordfence/">
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <th><?php esc_html_e('Package type:', 'wpfanyi-import'); ?></th>
+                  <td>
+                    <label>
+                      <input type="radio" name="trans_type" value="plugin" checked/>
+                        <?php esc_html_e('Plugin', 'wpfanyi-import'); ?></label>
+                    <label>
+                      <input type="radio" name="trans_type" value="theme"/>
+                        <?php esc_html_e('Theme', 'wpfanyi-import'); ?></label>
+                  </td>
+                </tr>
+                </tbody>
+              </table>
+              <p class="submit">
+                <input type="submit" name="submit" class="button-primary"
+                       value="<?php esc_html_e('Import', 'wpfanyi-import'); ?>"/>
+              </p>
+            </form>
+          </div>
         </div>
-				  
-				  </td>
-            </tr>
-            <tr>
-              <th><?php esc_html_e('Package type:', 'wpfanyi-import'); ?></th>
-              <td>
-                <label>
-                  <input type="radio" name="trans_type" value="plugin" checked/>
-                      <?php esc_html_e('Plugin', 'wpfanyi-import'); ?></label>
-                <label>
-                  <input type="radio" name="trans_type" value="theme"/>
-                    <?php esc_html_e('Theme', 'wpfanyi-import'); ?></label>
-              </td>
-            </tr>
-            </tbody>
-          </table>
-          <p class="submit">
-            <input type="submit" name="submit" class="button-primary"
-                   value="<?php esc_html_e('Import', 'wpfanyi-import'); ?>"/>
-          </p>
-        </form>
+      </div>
+      <div class="help">
+        <div class="layui-collapse" lay-accordion>
+          <div class="layui-colla-item">
+            <h2 class="layui-colla-title">一、通用手动安装</h2>
+            <div class="layui-colla-content layui-show"> 1.&nbsp;安装插件语言包<br />
+              将下载到的插件语言包&nbsp;<code>.zip</code>&nbsp;文件解压后上传至路径为您的网站<code>/wp-content/languages/plugins</code>即可。<br />
+              2.&nbsp;安装主题语言包<br />
+              将下载到的主题语言包&nbsp;<code>.zip</code>&nbsp;文件解压后上传至路径为您的网站<code>/wp-content/languages/themes</code>即可。<br />
+              3.&nbsp;其他安装方式<br />
+              除前两种方式外，也可以直接在插件或主题的安装文件夹内查找如&nbsp;<code>languages&nbsp;local&nbsp;lang&nbsp;i18n</code>&nbsp;放入对应的目录中。</div>
+          </div>
+          <div class="layui-colla-item">
+            <h2 class="layui-colla-title"> 二、文派插件安装</h2>
+            <div class="layui-colla-content"> 1、上传安装<br />
+              通过直接上传您已下载到的<code>XXX_zh_CN.zip</code>&nbsp;语言包文件，选择对应的语言类型【插件】或【主题】点击导入，即可完成。<br />
+              2、在线安装<br />
+              前往文派翻译（<a href="wpfanyi.com" target="_blank" rel="noopener">wpfanyi.com</a>）获取到语言包地址，填入在线安装地址，选择【插件】或【主题】点击导入，即可完成。</div>
+          </div>
+          <div class="layui-colla-item">
+            <h2 class="layui-colla-title">三、中文翻译请求</h2>
+            <div class="layui-colla-content">1、用户中文翻译请求<br />
+              &nbsp;如果您是WordPress插件主题作者，并希望将自己喜欢的产品翻译至中文，请通过&nbsp;<a href="https://wpfanyi.com/new-project" target="_blank" rel="noopener">https://wpfanyi.com/new-project</a>&nbsp;提交信息，我们将无偿处理。<br />
+              2、作者中文翻译请求<br />
+              如果您是WordPress开发者，并希望将自己的产品翻译至中文，寻求中文贡献者，请邮件至&nbsp;<a href="mailto:wpfanyi@feibisi.com" target="_blank" rel="noopener">wpfanyi@feibisi.com</a>&nbsp;我们中文翻译团队，将会无偿处理。（仅限&nbsp;WordPress.org&nbsp;免费产品，如需长期贡献者，请为&nbsp;<code>@wpfanyi</code>&nbsp;添加&nbsp;PET&nbsp;翻译权限）</p></div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
